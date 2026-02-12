8793faeba26f68eceb38880f2bf4b180e56ca015
lizb <loo4love90@gmail.com>
2026-02-12 13:48:35 +0900

    update

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -12,12 +12,15 @@ onDelete
     const onChangeCheckbox =()=>{
       onUpdate(id)
     }
+    const onClickDeleteButton=()=>{
+      onDelete(id)
+    }
   return (
-    <div className='TodoItem'>
+    <div className={`TodoItem ${isDone? 'done':''}`}>
       <input type="checkbox" checked={isDone}  onChange={onChangeCheckbox}/>
       <div className="content">{content}</div>
       <div className="date">{new Date(date).toLocaleDateString()}</div>
-      <button>삭제</button>
+      <button onClick={onClickDeleteButton}>삭제</button>
     </div>
   )
 }
