06d172e7408b6eb27e0b760d3e25ffdddd0635eb
lizb <loo4love90@gmail.com>
2026-02-12 13:11:53 +0900

    update

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./TodoItem.css"
+const TodoItem = ({
+  id,
+  isDone,
+  content,
+  date,
+  onUpdate ,
+onDelete
+}) => {
+
+    const onChangeCheckbox =()=>{
+      onUpdate(id)
+    }
+  return (
+    <div className='TodoItem'>
+      <input type="checkbox" checked={isDone}  onChange={onChangeCheckbox}/>
+      <div className="content">{content}</div>
+      <div className="date">{new Date(date).toLocaleDateString()}</div>
+      <button>삭제</button>
+    </div>
+  )
+}
+
+export default TodoItem
